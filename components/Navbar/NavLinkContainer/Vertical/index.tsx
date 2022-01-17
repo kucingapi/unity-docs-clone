fa4e72c216b7599fd79153e46bd7adb58dfbc73a
Novel Bafagih <novel.bafagih@udana.id>
2022-01-17 08:51:37 +0700

    formatting document

--- components/Navbar/NavLinkContainer/Vertical/index.tsx
+++ components/Navbar/NavLinkContainer/Vertical/index.tsx
@@ -1,7 +1,11 @@
-import { Transition } from "@headlessui/react";
-import { SidebarProvider } from "../../../../contexts/navbarContext/sidebarContext";
-import { ButtonLink } from "./ButtonLink";
-import { ButtonLinkDropdown } from "./ButtonLinkDropdown";
+import { Transition } from '@headlessui/react';
+import { useContext } from 'react';
+import {
+  SidebarContext,
+  SidebarProvider,
+} from '../../../../contexts/navbarContext/sidebarContext';
+import { ButtonLink } from './ButtonLink';
+import { ButtonLinkDropdown } from './ButtonLinkDropdown';
 
 interface NavlinkContainerProps {
   navLinks: Array<Object>;
@@ -12,49 +16,49 @@ interface NavlinkContainerProps {
 export const NavLinkContainerVertical = ({
   navLinks,
   sidebar,
-  sidebarHandler
+  sidebarHandler,
 }: NavlinkContainerProps) => {
-return(
-	<>
-		<Transition
-		show={sidebar != -1}
-		enter="transition-translate-x duration-300"
-		enterFrom="-translate-x-full"
-		enterTo="translate-x-0"
-		leave="transition-translate-x duration-300"
-		leaveFrom="translate-x-0"
-		leaveTo="-translate-x-full"
-		className='flex z-10 flex-col absolute gap-3 h-screen w-96 bg-black opacity-90 top-20 left-0 p-1 px-3'
-		>
-		{navLinks.map((navLink: any, index) => {
-			if(!navLink.dropdowns){
-			return (
-				<ButtonLink
-				link={navLink.link}
-				title={navLink.title}
-				key={index}
-				/>
-			);
-			}
-			else{
-			return (
-				<ButtonLinkDropdown
-				link={navLink.link}
-				title={navLink.title}
-				id={index + 1}
-				sidebarHandler={sidebarHandler} 
-				dropdowns={navLink.dropdowns}
-				sidebar={sidebar}
-				key={index}
-				/>
-			)
-			}
-		})}
-		</Transition>
-		<Transition
-			show={sidebar != -1 }
-			className='flex flex-col absolute gap-3 h-screen w-full z-0 bg-black opacity-40 top-20 right-0 p-1 px-3'
-		/>
-	</>
-)
+  const sidebarContext = useContext(SidebarContext);
+  return (
+    <>
+      <Transition
+        show={sidebar != -1}
+        enter="transition-translate-x duration-300"
+        enterFrom="-translate-x-full"
+        enterTo="translate-x-0"
+        leave="transition-translate-x duration-300"
+        leaveFrom="translate-x-0"
+        leaveTo="-translate-x-full"
+        className="flex z-10 flex-col absolute gap-3 h-screen w-96 bg-black opacity-90 top-20 left-0 p-1 px-3"
+      >
+        {navLinks.map((navLink: any, index) => {
+          if (!navLink.dropdowns) {
+            return (
+              <ButtonLink
+                link={navLink.link}
+                title={navLink.title}
+                key={index}
+              />
+            );
+          } else {
+            return (
+              <ButtonLinkDropdown
+                link={navLink.link}
+                title={navLink.title}
+                id={index + 1}
+                sidebarHandler={sidebarHandler}
+                dropdowns={navLink.dropdowns}
+                sidebar={sidebar}
+                key={index}
+              />
+            );
+          }
+        })}
+      </Transition>
+      <Transition
+        show={sidebar != -1}
+        className="flex flex-col absolute gap-3 h-screen w-full z-0 bg-black opacity-40 top-20 right-0 p-1 px-3"
+      />
+    </>
+  );
 };

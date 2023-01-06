be41b23ac303e8708268132a0e610ad134139a8e
noveel bafagih <novel.bafagih@udana.id>
2023-01-06 15:00:31 +0700

    [ADD] intersection observer for active components in sidebar

--- components/Home/SideBar/index.tsx
+++ components/Home/SideBar/index.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { FC } from 'react';
 
-export const SideBar = () => {
-  return (
-	<div>SideBar</div>
-  )
+interface SidebarProps {
+  activeList: number;
 }
+
+export const SideBar: FC<SidebarProps> = ({ activeList }) => {
+  const sidebarListData = [
+    'Unity Editor & Scripting',
+    'Real-time 3D Creation Tools',
+    'Unity Gaming Services',
+    'Premium Runtimes',
+  ];
+  console.log(activeList);
+
+  return (
+    <div className='sticky top-28 hidden lg:block'>
+      <p className="text-xs text-gray-500">CONTENT</p>
+      <ul className="flex flex-col gap-2 pl-4 pt-3 text-sm font-medium text-gray-400">
+        {sidebarListData.map((value, index) => (
+          <SideBarList active={activeList === index}>{value}</SideBarList>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+interface SidebarList {
+  active: boolean;
+}
+
+const SideBarList: FC<SidebarList> = ({ children, active }) => (
+  <li
+    className={`hover:text-gray-900 cursor-pointer ${
+      active && 'text-gray-900'
+    }`}
+  >
+    {children}
+  </li>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { MainTitle } from '../components/Home/MainTitle';
 import { PremiumSection } from '../components/Home/PremiumSection';
 import { RealtimeSection } from '../components/Home/RealtimeSection';
 import { ScriptingSection } from '../components/Home/ScriptingSection';
 import { ServiceSection } from '../components/Home/ServiceSection';
 import { SideBar } from '../components/Home/SideBar';
+import { InView } from 'react-intersection-observer';
 
 export default function Home() {
+  const [activeComponent, setActiveComponent] = useState(0);
+  const intersectionHandler = (inView: boolean, id: number) => {
+    if (inView) {
+      setActiveComponent(id);
+    }
+  };
   return (
     <>
       <main className="w-full p-14 flex-col-reverse flex lg:flex-row">
@@ -14,13 +22,33 @@ export default function Home() {
           itemType="https://schema.org/documentation"
         >
           <MainTitle />
-          <ScriptingSection />
-          <RealtimeSection />
-          <ServiceSection />
-          <PremiumSection />
+          <InView
+            as="div"
+            onChange={(inView) => intersectionHandler(inView, 0)}
+          >
+            <ScriptingSection />
+          </InView>
+          <InView
+            as="div"
+            onChange={(inView) => intersectionHandler(inView, 1)}
+          >
+            <RealtimeSection />
+          </InView>
+          <InView
+            as="div"
+            onChange={(inView) => intersectionHandler(inView, 2)}
+          >
+            <ServiceSection />
+          </InView>
+          <InView
+            as="div"
+            onChange={(inView) => intersectionHandler(inView, 3)}
+          >
+            <PremiumSection />
+          </InView>
         </article>
         <div className="lg:w-3/12 p-2">
-          <SideBar />
+          <SideBar activeList={activeComponent}  />
         </div>
       </main>
     </>
